a5872e1d9171a876f79435c83a73c0a7f591fc9b
Bishal Thapaliya <vishal.thapaliya@gmail.com>
2018-06-28 00:38:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,12 +45,12 @@ $(document).ready(function () {
   })
 
 
-  //ripples
+ /* //ripples
   $("#header, .info").ripples({
     dropRadius: 25,
     perturbance: 0.6,
   });
-
+*/
   //magnific popup
   $(".parent-container").magnificPopup({
     delegate: 'a', // child items selector, by clicking on it popup will open
